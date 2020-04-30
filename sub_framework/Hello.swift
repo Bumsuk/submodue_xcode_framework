03b24b03d665638b1a3bb929785d16fcb4c2d085
brown <xenix72@gmail.com>
2020-04-30 16:08:34 +0900

    public class Hello added

---
+++ sub_framework/Hello.swift
@@ -0,0 +1,15 @@
+//
+//  Hello.swift
+//  sub_framework
+//
+//  Created by brown on 2020/04/30.
+//  Copyright © 2020 brown. All rights reserved.
+//
+
+import Foundation
+
+public class Hello {
+	public class func sayHo() {
+		print("[🤡] syaHo~~~~~ 세이 호우!")
+	}
+}
