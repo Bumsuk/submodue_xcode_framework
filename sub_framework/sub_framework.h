ab95094ba8275e58fc761d0942dd54a78519db65
brown <xenix72@gmail.com>
2020-04-30 15:57:23 +0900

    first sub_framework project files added!

---
+++ sub_framework/sub_framework.h
@@ -0,0 +1,19 @@
+//
+//  sub_framework.h
+//  sub_framework
+//
+//  Created by brown on 2020/04/30.
+//  Copyright © 2020 brown. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for sub_framework.
+FOUNDATION_EXPORT double sub_frameworkVersionNumber;
+
+//! Project version string for sub_framework.
+FOUNDATION_EXPORT const unsigned char sub_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sub_framework/PublicHeader.h>
+
+
